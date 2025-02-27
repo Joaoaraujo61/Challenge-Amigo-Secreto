c5ec355d0882a896e974702a3052d3fb7e587cd4
Joaoaraujo61 <joaopedrodearaujo61@gmail.com>
2025-02-26 22:08:04 -0300

    adicionar amigos function added

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+const butaoAdd = document.querySelector('.button-add')
+const butaoSortear = document.querySelector('.button-draw')
+const sorteado = document.querySelector('#resultado')
+
+let arrayaAmigos = []
+
+function adicionarAmigo() { 
+    const inputAmigo = document.querySelector('#amigo')
+    const listaAmigos = document.querySelector('#listaAmigos')
+    
+    if(!inputAmigo.value || inputAmigo.value.trim().length === 0 || !isNaN(inputAmigo.value)){
+        window.alert('Valor inválido!')
+    }else{
+        arrayaAmigos.push(String(inputAmigo.value))
+    }
+
+    listaAmigos.innerHTML = arrayaAmigos.join('<br/>')
+}
+
+function sortearAmigo(){
+
+}
